cc43d55883c19e51de0f5c83ac4ae23205fedc77
Raininstone <raininstone@users.noreply.github.com>
2016-11-21 20:32:49 +0000

    TestCommit
    Add comment "Git Commit"

---
+++ src/concertbooking/ConcertBooking.java
@@ -0,0 +1,22 @@
+
+package concertbooking;
+
+/**
+ *
+ * @author B00663255
+ */
+public class ConcertBooking
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        ConcertUI myUI = new ConcertUI();
+        myUI.setVisible(true);
+        
+        //Git commit
+    }
+    
+}
